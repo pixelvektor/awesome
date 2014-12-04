8d8ca1186a6209bf00cc274c92314e572f032202
Fabian <fabian.schschneider@googlemail.com>
2014-12-04 22:05:37 +0100

    Initiale SC Dateien

---
+++ awesome/src/Awesome.java
@@ -0,0 +1,21 @@
+/** Hochschule Hamm-Lippstadt
+ * Praktikum Informatik 1 (Awesome App)
+ * (C) 2014 Matthias Ridder, Dominic von Zielinski, Adrian Schmidt, Fabian Schneider
+ * 04.12.2014
+ */
+
+/**
+ * @author 
+ *
+ */
+public class Awesome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ awesome/src/data/Spieler.java
@@ -0,0 +1,14 @@
+/** Hochschule Hamm-Lippstadt
+ * Praktikum Informatik 1 (Awesome App)
+ * (C) 2014 Matthias Ridder, Dominic von Zielinski, Adrian Schmidt, Fabian Schneider
+ * 04.12.2014
+ */
+package data;
+
+/**
+ * @author
+ *
+ */
+public class Spieler {
+
+}
