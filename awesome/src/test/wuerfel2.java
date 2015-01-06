82af9721f8cbb3bd63ee8759a4a0eeaab7bf587d
Twyxt95 <a.schmidt0101@web.de>
2015-01-06 14:10:41 +0100

    Feld löschen
    feld löschen funktioniert

--- awesome/src/test/wuerfel2.java
+++ awesome/src/test/wuerfel2.java
@@ -13,7 +13,7 @@ public class wuerfel2
  public wuerfel2()
  {
 	 String name;
-	 //String name2;
+	 String name2;
 		
 	 final Spieler[] spieler = new Spieler[2];
 	  Kasten[] kaesten = new Kasten[9];
@@ -26,14 +26,16 @@ public class wuerfel2
 			kaesten[x-3].setFelder(kaesten[x-3].erzeugeFeld());	// Erzeugt die Felder in den Kaesten
 		}
 		    name = "Erika";
-			//name2 = "Klaus";
+			name2 = "Klaus";
 			spieler[0] = new Spieler(name);
-			//spieler[1] = new Spieler(name2);
+			spieler[1] = new Spieler(name2);
 			System.out.println("Name Spieler 1: " + spieler[0].getName());
-			//System.out.println("Name Spieler 2: " + spieler[1].getName());
-	spieler[0].setWuerfelErgebnis(2);
+			System.out.println("Name Spieler 2: " + spieler[1].getName());
+	spieler[0].setWuerfelErgebnis(5);
 	System.out.println(spieler[0].getWuerfelErgebnis());
 	spieler[0].pinSetzen(kaesten);
-	 
+	spieler[1].setWuerfelErgebnis(2);
+	System.out.println(spieler[1].getWuerfelErgebnis());
+	spieler[1].pinSetzen(kaesten);
  }
 }
