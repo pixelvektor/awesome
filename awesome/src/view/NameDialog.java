3007431da4090db69fae2c2d53cab500a8f155d2
Fabian <fabian.schschneider@googlemail.com>
2015-01-10 20:28:48 +0100

    Bugfix.

--- awesome/src/view/NameDialog.java
+++ awesome/src/view/NameDialog.java
@@ -6,14 +6,13 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Frame;
-import java.awt.color.*;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
